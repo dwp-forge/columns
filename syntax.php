b625f62dfad4fa06a86209c6f59cd91980801364
Mykola Ostrovskyy <spambox03@mail.ru>
2009-02-28 12:59:51 +0000

    r78
    * Plugin type is changed from container to substitution
    * Enter/exit patterns are replaced by special patterns
    * Dummy rendering (HTML comments)
    - Structure parsing code (to be moved into action plugin)

--- syntax.php
+++ syntax.php
@@ -29,6 +29,30 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
      */
     function syntax_plugin_columns() {
         $this->mode = substr(get_class($this), 7);
+
+        $columns = $this->_getColumnsTagName();
+        $newColumn = $this->_getNewColumnTagName();
+        if ($this->getConf('wrapnewcol') == 1) {
+            $newColumnLexer = '<' . $newColumn . '(?:>|\s.*?>)';
+            $newColumnHandler = '<' . $newColumn . '(.*?)>';
+        }
+        else {
+            $newColumnLexer = $newColumn;
+            $newColumnHandler = $newColumn;
+        }
+        $enterLexer = '<' . $columns . '(?:>|\s.*?>)';
+        $enterHandler = '<' . $columns . '(.*?)>';
+        $exit = '<\/' . $columns . '>';
+        $lookAhead = '(?=.*?' . $exit . ')';
+
+        $this->lexerSyntax['enter'] = $enterLexer . '\n?' . $lookAhead;
+        $this->lexerSyntax['newcol'] = '\n?' . $newColumnLexer . '\n?' . $lookAhead;
+        $this->lexerSyntax['exit'] = '\n?' . $exit;
+
+        $this->syntax[DOKU_LEXER_ENTER] = '/' . $enterHandler . '/';
+        $this->syntax[DOKU_LEXER_MATCHED] = '/' . $newColumnHandler . '/';
+        $this->syntax[DOKU_LEXER_EXIT] = '/' . $exit . '/';
+
         $this->block = 0;
         $this->nextBlock = 0;
         $this->nestingLevel = 0;
@@ -54,36 +78,11 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
      * What kind of syntax are we?
      */
     function getType() {
-        return 'container';
+        return 'substition';
     }
 
     function getPType() {
         return 'block';
-    }
-
-    /**
-     * What modes are allowed within our mode?
-     */
-    function accepts($mode) {
-        /* Ensure that our own mode can be nested */
-        if ($mode == $this->mode) {
-            return true;
-        }
-        return parent::accepts($mode);
-    }
-
-    /**
-     * What modes are allowed within our mode?
-     */
-    function getAllowedTypes() {
-        return array (
-            'container',
-            'substition',
-            'protected',
-            'disabled',
-            'formatting',
-            'paragraphs'
-        );
     }
 
     /**
@@ -94,31 +93,27 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
     }
 
     function connectTo($mode) {
-        $columnsTag = $this->_getColumnsTagName();
-        $this->Lexer->addEntryPattern('<' . $columnsTag . '.*?>(?=.*?</' . $columnsTag . '>)', $mode, $this->mode);
-        $this->Lexer->addPattern($this->_getNewColumnTag(), $this->mode);
-    }
-
-    function postConnect() {
-        $this->Lexer->addExitPattern('</' . $this->_getColumnsTagName() . '>', $this->mode);
+        $this->Lexer->addSpecialPattern($this->lexerSyntax['enter'], $mode, $this->mode);
+        $this->Lexer->addSpecialPattern($this->lexerSyntax['newcol'], $mode, $this->mode);
+        $this->Lexer->addSpecialPattern($this->lexerSyntax['exit'], $mode, $this->mode);
     }
 
     /**
      * Handle the match
      */
     function handle($match, $state, $pos, &$handler) {
+        foreach ($this->syntax as $state => $pattern) {
+            if (preg_match($pattern, $match, $data) == 1) {
+                break;
+            }
+        }
         switch ($state) {
             case DOKU_LEXER_ENTER:
-                return $this->_handleEnter($match);
-
             case DOKU_LEXER_MATCHED:
-                return $this->_handleMatched();
-
-            case DOKU_LEXER_UNMATCHED:
-                return array($state, $match);
+                return array($state, preg_split('/\s+/', $data[1], -1, PREG_SPLIT_NO_EMPTY));
 
             case DOKU_LEXER_EXIT:
-                return $this->_handleExit();
+                return array($state);
         }
         return false;
     }
@@ -130,27 +125,15 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
         if ($mode == 'xhtml') {
             switch ($data[0]) {
                 case DOKU_LEXER_ENTER:
-                    $this->_renderEnter($renderer, $data[1], $data[2]);
+                    $this->_renderEnter($renderer, $data[1]);
                     break;
 
                 case DOKU_LEXER_MATCHED:
-                    $this->_renderMatched($renderer, $data[1], $data[2]);
-                    break;
-
-                case DOKU_LEXER_UNMATCHED:
-                    $renderer->doc .= $renderer->_xmlEntities($data[1]);
+                    $this->_renderMatched($renderer, $data[1]);
                     break;
 
                 case DOKU_LEXER_EXIT:
-                    $renderer->doc .= '</td></tr></table>';
-                    break;
-            }
-            return true;
-        }
-        elseif ($mode == 'metadata') {
-            switch ($data[0]) {
-                case DOKU_LEXER_EXIT:
-                    $renderer->meta['columns'][$data[1]] = $data[2];
+                    $renderer->doc .= '<!-- /columns -->';
                     break;
             }
             return true;
@@ -159,45 +142,7 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
     }
 
     /**
-     */
-    function _handleEnter($match) {
-        $this->block = ++$this->nextBlock;
-        $this->nestedBlock[++$this->nestingLevel] = $this->block;
-        $this->columns[$this->block] = 1;
-
-        $width['table'] = '-';
-        $width['col'] = array();
-
-        preg_match('/<' . $this->_getColumnsTagName() . '(.*?)>/', $match, $match);
-
-        if (array_key_exists(1, $match)) {
-            $temp = preg_split('/\s+/', $match[1], -1, PREG_SPLIT_NO_EMPTY);
-
-            if (count($temp) > 0) {
-                $width['table'] = array_shift($temp);
-                $width['col'] = $temp;
-            }
-        }
-        return array(DOKU_LEXER_ENTER, $this->block, $width);
-    }
-
-    /**
-     */
-    function _handleMatched() {
-        $this->columns[$this->block]++;
-        return array(DOKU_LEXER_MATCHED, $this->block, $this->columns[$this->block]);
-    }
-
-    /**
-     */
-    function _handleExit() {
-        $result = array(DOKU_LEXER_EXIT, $this->block, $this->columns[$this->block]);
-        $this->block = $this->nestedBlock[--$this->nestingLevel];
-        return $result;
-    }
-
-    /**
-     * Returns columns tag without the brackets
+     * Returns columns tag
      */
     function _getColumnsTagName() {
         $tag = $this->getConf('kwcolumns');
@@ -210,13 +155,10 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
     /**
      * Returns new column tag
      */
-    function _getNewColumnTag() {
+    function _getNewColumnTagName() {
         $tag = $this->getConf('kwnewcol');
         if ($tag == '') {
             $tag = $this->getLang('kwnewcol');
-        }
-        if ($this->getConf('wrapnewcol') == 1) {
-            $tag = '<' . $tag . '>';
         }
         return $tag;
     }
@@ -224,79 +166,22 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
     /**
      * Renders table and col tags, starts the table with first column
      */
-    function _renderEnter(&$renderer, $block, $width) {
-        $renderer->doc .= $this->_renderTable($width['table']);
-
-        $columns = $this->_getColumns($block);
-        $colWidth = $width['col'];
-
-        if (count($colWidth) < $columns) {
-            $colWidth = array_pad($colWidth, $columns, '-');
+    function _renderEnter(&$renderer, $attribute) {
+        $renderer->doc .= '<!-- columns';
+        foreach($attribute as $a) {
+            $renderer->doc .= ' ' . $a;
         }
-
-        $column = 0;
-        $this->align = array();
-
-        foreach ($colWidth as $width) {
-            $this->align[++$column] = $this->_getAlignment($width);
-            $renderer->doc .= $this->_renderCol(trim($width, '*'));
-        }
-
-        $renderer->doc .= '<tr>' . $this->_renderTd($this->align[1], 'first');
+        $renderer->doc .= ' -->';
     }
 
     /**
      */
-    function _renderMatched(&$renderer, $block, $column) {
-        $html = '</td>';
-
-        $class = '';
-        if ($column == $this->_getColumns($block)) {
-            $class = 'last';
+    function _renderMatched(&$renderer, $attribute) {
+        $renderer->doc .= '<!-- newcolumn';
+        foreach($attribute as $a) {
+            $renderer->doc .= ' ' . $a;
         }
-
-        $html .= $this->_renderTd($this->align[$column], $class);
-
-        /* HACK: remove extra paragrap tags around new column tags */
-        if (strstr(substr($renderer->doc, -5), '<p>') !== false) {
-            $renderer->doc .= '</p>' . $html . '<p>';
-        }
-        else {
-            $renderer->doc .= $html;
-        }
-    }
-
-    /**
-     * Returns number of columns in specified block
-     */
-    function _getColumns($block) {
-        if (empty($this->columns)) {
-            global $ID;
-
-            $this->columns = p_get_metadata($ID, 'columns');
-        }
-        return $this->columns[$block];
-    }
-
-    /**
-     * Returns column text alignment
-     */
-    function _getAlignment($width) {
-        preg_match('/^(\*?).*?(\*?)$/', $width, $match);
-        $align = $match[1] . '-' . $match[2];
-        switch ($align) {
-            case '-':
-                return '';
-
-            case '-*':
-                return 'left';
-
-            case '*-':
-                return 'right';
-
-            case '*-*':
-                return 'center';
-        }
+        $renderer->doc .= ' -->';
     }
 
     /**
@@ -307,17 +192,6 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
         }
         else {
             return '<table class="columns-plugin" style="width:' . $width . '">';
-        }
-    }
-
-    /**
-     */
-    function _renderCol($width) {
-        if ($width == '-') {
-            return '<col>';
-        }
-        else {
-            return '<col style="width:' . $width . '">';
         }
     }
 
