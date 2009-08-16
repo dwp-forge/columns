67cf4647d093815d6c2c364d9296281e235d4b31
Mykola Ostrovskyy <spambox03@mail.ru>
2009-08-16 13:04:26 +0000

    r230: Added basic ODT rendering

--- syntax.php
+++ syntax.php
@@ -122,6 +122,28 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
             }
             return true;
         }
+        else if ($mode == 'odt') {
+            switch ($data[0]) {
+                case DOKU_LEXER_ENTER:
+                    $this->_addOdtTableStyle($renderer, $data[1]);
+                    $this->_addOdtColumnStyles($renderer, $data[1]);
+                    $this->_renderOdtTableEnter($renderer, $data[1]);
+                    $this->_renderOdtColumnEnter($renderer, $data[1]);
+                    break;
+
+                case DOKU_LEXER_MATCHED:
+                    $this->_addOdtColumnStyles($renderer, $data[1]);
+                    $this->_renderOdtColumnExit($renderer);
+                    $this->_renderOdtColumnEnter($renderer, $data[1]);
+                    break;
+
+                case DOKU_LEXER_EXIT:
+                    $this->_renderOdtColumnExit($renderer);
+                    $this->_renderOdtTableExit($renderer);
+                    break;
+            }
+            return true;
+        }
         return false;
     }
 
@@ -209,5 +231,202 @@ class syntax_plugin_columns extends DokuWiki_Syntax_Plugin {
             $result = $attribute[$name];
         }
         return $result;
+    }
+
+    /**
+     *
+     */
+    function _renderOdtTableEnter(&$renderer, $attribute) {
+        $columns = $this->_getAttribute($attribute, 'columns');
+        $blockId = $this->_getAttribute($attribute, 'block-id');
+        $styleName = $this->_getOdtTableStyleName($blockId);
+
+        $renderer->doc .= '<table:table table:style-name="' . $styleName . '">';
+        for ($c = 0; $c < $columns; $c++) {
+            $styleName = $this->_getOdtTableStyleName($blockId, $c + 1);
+            $renderer->doc .= '<table:table-column table:style-name="' . $styleName . '" />';
+        }
+        $renderer->doc .= '<table:table-row>';
+    }
+
+    /**
+     *
+     */
+    function _renderOdtColumnEnter(&$renderer, $attribute) {
+        $blockId = $this->_getAttribute($attribute, 'block-id');
+        $columnId = $this->_getAttribute($attribute, 'column-id');
+        $styleName = $this->_getOdtTableStyleName($blockId, $columnId, 1);
+        $renderer->doc .= '<table:table-cell table:style-name="' . $styleName . '" office:value-type="string">';
+    }
+
+    /**
+     *
+     */
+    function _renderOdtColumnExit(&$renderer) {
+        $renderer->doc .= '</table:table-cell>';
+    }
+
+    /**
+     *
+     */
+    function _renderOdtTableExit(&$renderer) {
+        $renderer->doc .= '</table:table-row>';
+        $renderer->doc .= '</table:table>';
+    }
+
+    /**
+     *
+     */
+    function _addOdtTableStyle(&$renderer, $attribute) {
+        $metrics = $this->_getOdtMetrics($renderer->autostyles);
+        $styleName = $this->_getOdtTableStyleName($this->_getAttribute($attribute, 'block-id'));
+        $style = '<style:style style:name="' . $styleName . '" style:family="table">';
+        $style .= '<style:table-properties';
+        $width = $this->_getAttribute($attribute, 'table-width');
+
+        if ($width != '') {
+            $style .= ' style:width="' . $this->_getOdtAbsoluteWidth($metrics, $width) . '"';
+        }
+        $align = ($width == '100%') ? 'margins' : 'left';
+        $style .= ' table:align="' . $align . '"/>';
+        $style .= '</style:style>';
+
+        $renderer->autostyles[$styleName] = $style;
+    }
+
+    /**
+     *
+     */
+    function _addOdtColumnStyles(&$renderer, $attribute) {
+        $metrics = $this->_getOdtMetrics($renderer->autostyles);
+        $blockId = $this->_getAttribute($attribute, 'block-id');
+        $columnId = $this->_getAttribute($attribute, 'column-id');
+        $styleName = $this->_getOdtTableStyleName($blockId, $columnId);
+
+        $style = '<style:style style:name="' . $styleName . '" style:family="table-column">';
+        $style .= '<style:table-column-properties';
+        $width = $this->_getAttribute($attribute, 'column-width');
+
+        if ($width != '') {
+            $style .= ' style:column-width="' . $this->_getOdtAbsoluteWidth($metrics, $width) . '"';
+        }
+        $style .= '/>';
+        $style .= '</style:style>';
+
+        $renderer->autostyles[$styleName] = $style;
+
+        $styleName = $this->_getOdtTableStyleName($blockId, $columnId, 1);
+
+        $style = '<style:style style:name="' . $styleName . '" style:family="table-cell">';
+        $style .= '<style:table-cell-properties';
+        $style .= ' fo:border="none"';
+        $style .= ' fo:padding-top="0cm"';
+        $style .= ' fo:padding-bottom="0cm"';
+
+        switch ($this->_getAttribute($attribute, 'class')) {
+            case 'first':
+                $style .= ' fo:padding-left="0cm"';
+                $style .= ' fo:padding-right="0.4cm"';
+                break;
+
+            case 'last':
+                $style .= ' fo:padding-left="0.4cm"';
+                $style .= ' fo:padding-right="0cm"';
+                break;
+        }
+
+        /* There seems to be no easy way to control horizontal alignment of text within
+           the column as fo:text-align aplies to individual paragraphs. */
+        //TODO: $this->_getAttribute($attribute, 'text-align');
+
+        $align = $this->_getAttribute($attribute, 'vertical-align');
+        if ($align != '') {
+            $style .= ' style:vertical-align="' . $align . '"';
+        }
+        else {
+            $style .= ' style:vertical-align="top"';
+        }
+
+        $style .= '/>';
+        $style .= '</style:style>';
+
+        $renderer->autostyles[$styleName] = $style;
+    }
+
+    /**
+     *
+     */
+    function _getOdtMetrics($autoStyle) {
+        $result = array();
+        if (array_key_exists('pm1', $autoStyle)) {
+            $style = $autoStyle['pm1'];
+            if (preg_match('/fo:page-width="([\d\.]+)(.+?)"/', $style, $match) == 1) {
+                $result['page-width'] = floatval($match[1]);
+                $result['page-width-units'] = $match[2];
+                $units = $match[2];
+
+                if (preg_match('/fo:margin-left="([\d\.]+)(.+?)"/', $style, $match) == 1) {
+                    // TODO: Unit conversion
+                    if ($match[2] == $units) {
+                        $result['page-width'] -= floatval($match[1]);
+                    }
+                }
+                if (preg_match('/fo:margin-right="([\d\.]+)(.+?)"/', $style, $match) == 1) {
+                    if ($match[2] == $units) {
+                        $result['page-width'] -= floatval($match[1]);
+                    }
+                }
+            }
+        }
+        if (!array_key_exists('page-width', $result)) {
+            $result['page-width'] = 17;
+            $result['page-width-units'] = 'cm';
+        }
+
+        /* There seems to be no way to get default font size apart from
+           loading styles.xml, which is expensive thing to do for every
+           columns block. */
+        $result['font-size'] = 12;
+        $result['font-size-units'] = 'pt';
+        return $result;
+    }
+
+    /**
+     *
+     */
+    function _getOdtTableStyleName($blockId, $columnId = 0, $cell = 0) {
+        $result = 'ColumnsBlock' . $blockId;
+        if ($columnId != 0) {
+            if ($columnId <= 26) {
+                $result .= '.' . chr(ord('A') + $columnId - 1);
+            }
+            else {
+                /* To unlikey to handle it properly */
+                $result .= '.a';
+            }
+            if ($cell != 0) {
+                $result .= $cell;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Convert relative units to absolute
+     */
+    function _getOdtAbsoluteWidth($metrics, $width) {
+        if (preg_match('/([\d\.]+)(.+)/', $width, $match) == 1) {
+            switch ($match[2]) {
+                case '%':
+                    /* Won't work for nested column blocks */
+                    $width = ($match[1] / 100 * $metrics['page-width']) . $metrics['page-width-units'];
+                    break;
+                case 'em':
+                    /* Rough estimate */
+                    $width = ($match[1] * 0.8 * $metrics['font-size']) . $metrics['font-size-units'];
+                    break;
+            }
+        }
+        return $width;
     }
 }
